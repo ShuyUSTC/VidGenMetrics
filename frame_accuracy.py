6a0bd7e4bb71e7d753d36acf1409ece2fa1d4f01
ShuyUSTC <shuy.ustc@gmail.com>
2023-06-28 15:43:17 +0800

    frame accuracy and frame consistency

---
+++ frame_accuracy.py
@@ -0,0 +1,90 @@
+"""
+Code for Video version of CLIPScore (https://arxiv.org/abs/2104.08718)
+
+By Yan Shu
+shuy.ustc@gmail.com
+"""
+from transformers import CLIPProcessor, CLIPModel
+
+import torch
+import numpy as np
+
+
+class FrameAcc:
+    RETURN_TYPE = [
+        'pt',
+        'np',
+        'float',
+    ]
+
+    def __init__(self, version="openai/clip-vit-base-patch32", device="cuda", prefix="A photo depicts",
+                 max_length=77, mini_bsz=64, return_type='pt'):
+        """Compute CLIP score between video frames and corresponding prompt
+        Args:
+            version: clip model version
+            device: device
+            prefix: prefix added to the prompt
+            max_length: max_length for text tokenizer
+            mini_bsz: mini batch size to process frames
+            return_type: return type. support: pt (torch.Tensor), np(numpy.Ndarray), float (float)
+        """
+        assert return_type in self.RETURN_TYPE, f'Got return type: {return_type}, but only support f{self.RETURN_TYPE}'
+        self.return_type = return_type
+        self.processor = CLIPProcessor.from_pretrained(version)
+        self.model = CLIPModel.from_pretrained(version).to(device)
+        self.prefix = prefix
+        self.device = device
+        self.max_length = max_length
+        self.mini_bsz = mini_bsz
+        self.freeze()
+
+    def freeze(self):
+        self.model = self.model.eval()
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    @torch.no_grad()
+    def __call__(self, texts, frames, w=2.5):
+        """
+        Compute CLIP score between frames and texts
+
+        Args:
+            texts (list[str] | str):
+            frames (list[Image | np.Ndarray] | Image | np.Ndarray):
+
+        Returns:
+            CLIP similarity(ies)
+
+        """
+        mini_bsz = self.mini_bsz if self.mini_bsz > 0 else len(frames)
+        mini_bsz = min(mini_bsz, len(frames))
+        clip_score_per_frame = []
+        for b_idx in range((len(frames) + mini_bsz - 1) // mini_bsz):
+            frame_batch = frames[b_idx * mini_bsz: b_idx * mini_bsz + mini_bsz]
+            inputs = self.processor(text=texts, images=frame_batch, truncation=True, max_length=self.max_length,
+                                    return_overflowing_tokens=False, padding="max_length",
+                                    return_tensors="pt")
+            inputs = inputs.to(self.device)
+            outputs = self.model(**inputs)
+            clip_score_per_frame.append(outputs.logits_per_image)
+        clip_score_per_frame = torch.cat(clip_score_per_frame)
+        clip_score = w * clip_score_per_frame.mean(dim=0).cpu()
+        if self.return_type == 'pt':
+            return clip_score
+        elif self.return_type == 'np':
+            return np.array(clip_score)
+        elif self.return_type == 'float':
+            return [float(sim) for sim in clip_score] if len(clip_score) > 1 else float(clip_score)
+        else:
+            raise NotImplementedError
+
+
+if __name__ == '__main__':
+    import glob
+    from PIL import Image
+    prompt = 'a silver jeep driving down a curvy road in the countryside.'
+    images = glob.glob('./examples/*.png')
+    images.sort()
+    images = [Image.open(img) for img in images]
+    frame_acc = FrameAcc()
+    print(frame_acc(prompt, images))
---
+++ frame_consistency.py
@@ -0,0 +1,77 @@
+"""
+Code for frame consistency
+By Yan Shu
+shuy.ustc@gmail.com
+"""
+import numpy as np
+import torch
+from torch.nn import CosineSimilarity
+from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
+
+
+class FrameConsistency:
+    RETURN_TYPE = [
+        'pt',
+        'np',
+        'float',
+    ]
+
+    def __init__(self, version="openai/clip-vit-base-patch32", device="cuda", mini_bsz=64, return_type='pt'):
+        """Measure frame consistency by computing the CLIP consine similarity of consecutive frames
+        Args:
+            version: clip model version
+            device: device
+            mini_bsz: mini batch size to process frames
+            return_type: return type. support: pt (torch.Tensor), np(numpy.Ndarray), float (float)
+        """
+        assert return_type in self.RETURN_TYPE, f'Got return type: {return_type}, but only support f{self.RETURN_TYPE}'
+        self.return_type = return_type
+        self.processor = CLIPImageProcessor.from_pretrained(version)
+        self.model = CLIPVisionModelWithProjection.from_pretrained(version).to(device)
+        self.cosine = CosineSimilarity(1).to(device)
+        self.device = device
+        self.mini_bsz = mini_bsz
+        self.freeze()
+
+    def freeze(self):
+        self.model = self.model.eval()
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    @torch.no_grad()
+    def __call__(self, frames):
+        """
+        Compute frame consistency of consecutive frames in `frames`
+
+        Args:
+            frames (list[Image | np.Ndarray] | Image | np.Ndarray):
+
+        Returns:
+            frame consistency
+
+        """
+        assert len(frames) > 1, 'Need at least 2 frames to compute frame consistency'
+        inputs = self.processor(frames, return_tensors='pt').to(self.device)
+        outputs = self.model(**inputs)
+        frame_embeds = outputs.image_embeds
+        consist = self.cosine(frame_embeds[:-1], frame_embeds[1:])
+        consist = consist.mean()
+
+        if self.return_type == 'pt':
+            return consist
+        elif self.return_type == 'np':
+            return np.array(consist)
+        elif self.return_type == 'float':
+            return [float(sim) for sim in consist] if len(consist) > 1 else float(consist)
+        else:
+            raise NotImplementedError
+
+
+if __name__ == '__main__':
+    import glob
+    from PIL import Image
+    images = glob.glob('./examples/*.png')
+    images.sort()
+    images = [Image.open(img) for img in images]
+    frame_consistency = FrameConsistency()
+    print(frame_consistency(images))
